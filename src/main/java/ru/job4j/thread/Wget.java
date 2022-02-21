9f3b4f41bd448feb5990b9daad026122509be8f1
Quertte <a.injen90@gmail.com>
2022-02-21 20:44:46 +0300

    4. Скачивание файла с ограничением. [#144271 #267475]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -7,26 +7,33 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String dest;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String dest) {
         this.url = url;
         this.speed = speed;
+        this.dest = dest;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fos = new FileOutputStream("pom_tmp.xml")) {
-            byte[] buffer = new byte[1024];
+             FileOutputStream fos = new FileOutputStream(dest)) {
+            byte[] buffer = new byte[speed];
             int bytesRead;
             long startTime = System.currentTimeMillis();
-            while ((bytesRead = in.read(buffer, 0, 1024)) != -1) {
-                long actualTime = System.currentTimeMillis() - startTime;
-                long expectedTime = bytesRead / speed;
-                fos.write(buffer, 0, bytesRead);
-                if (actualTime < expectedTime) {
-                    Thread.sleep(expectedTime - actualTime);
+            int downloadData = 0;
+            while ((bytesRead = in.read(buffer, 0, speed)) != -1) {
+                downloadData += bytesRead;
+                if (downloadData == speed) {
+                    long finishTime = System.currentTimeMillis() - startTime;
+                    System.out.println(finishTime);
+                    if (finishTime < 1000) {
+                        Thread.sleep(1000 - finishTime);
+                        downloadData = 0;
+                    }
                 }
+                fos.write(buffer, 0, bytesRead);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -34,9 +41,13 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Error");
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String dest = args[2];
+        Thread wget = new Thread(new Wget(url, speed, dest));
         wget.start();
         wget.join();
     }
