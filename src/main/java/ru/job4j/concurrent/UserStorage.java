0a01ae43cbcc27bc96dd635c47aa1ff777822fc2
Quertte <a.injen90@gmail.com>
2022-02-24 18:33:47 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #268591]

--- src/main/java/ru/job4j/concurrent/UserStorage.java
+++ src/main/java/ru/job4j/concurrent/UserStorage.java
@@ -10,30 +10,15 @@ public class UserStorage implements Storage, Transfer {
     private final ConcurrentHashMap<Integer, User> store = new ConcurrentHashMap<>();
 
     public synchronized boolean add(User user) {
-        boolean rsl = false;
-        if (!store.containsValue(user)) {
-            store.put(user.getId(), user);
-            rsl = true;
-        }
-        return rsl;
+        return store.putIfAbsent(user.getId(), user) == null;
     }
 
     public synchronized boolean update(User user) {
-        boolean rsl = false;
-        if (store.containsValue(user)) {
-            store.put(user.getId(), user);
-            rsl = true;
-        }
-        return rsl;
+        return store.replace(user.getId(), user) != null;
     }
 
     public synchronized boolean delete(User user) {
-        boolean rsl = false;
-        if (store.containsValue(user)) {
-            store.remove(user.getId());
-            rsl = true;
-        }
-        return rsl;
+        return store.remove(user.getId(), user);
     }
 
     @Override
@@ -41,8 +26,8 @@ public class UserStorage implements Storage, Transfer {
         boolean rsl = false;
         if (store.containsKey(fromId) && store.containsKey(toId)) {
             User user = store.get(fromId);
-            User user1 = store.get(fromId);
-            if (user.getAmount() >= amount) {
+            User user1 = store.get(toId);
+            if (user != null && user1 != null && user.getAmount() >= amount) {
                 user.setAmount(user.getAmount() - amount);
                 user1.setAmount(user1.getAmount() + amount);
                 rsl = true;
