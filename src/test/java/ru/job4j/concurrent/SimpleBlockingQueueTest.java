87d536004500346e50ca69f4123fc65da6832cc6
Quertte <a.injen90@gmail.com>
2022-02-27 10:45:47 +0300

    1. Реализовать шаблон Producer Consumer. fix3

--- src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -9,33 +9,42 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleBlockingQueueTest {
-    @Test
-    public void test() throws InterruptedException {
-        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
-        AtomicInteger count = new AtomicInteger(1);
-        Thread producer = new Thread(
-                () -> {
-                    for (int i = 0; i < queue.getLimit(); i++) {
-                        try {
-                            queue.offer(count.getAndIncrement());
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
+    private final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
+    private final AtomicInteger count = new AtomicInteger(1);
+    private final AtomicReference<Integer> value = new AtomicReference<>();
+
+    private class Producer extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < queue.getLimit(); i++) {
+                try {
+                    queue.offer(count.getAndIncrement());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
-        );
-        AtomicReference<Integer> value = new AtomicReference<>();
-        Thread consumer = new Thread(() -> {
+            }
+        }
+    }
+
+    private class Consumer extends Thread {
+        @Override
+        public void run() {
             try {
                 value.set(queue.poll());
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-        });
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        Thread producer = new Producer();
+        Thread consumer = new Consumer();
         producer.start();
         consumer.start();
         producer.join();
-        producer.join();
+        consumer.join();
         assertThat(queue.size(), is(3));
         assertThat(value.get(), is(1));
     }
